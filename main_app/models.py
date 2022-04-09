ff469454dbeb5917f6e539cdf72bc87ad3821153
ekeith <evanmkeith@gmail.com>
2022-04-09 13:03:12 -0700

    addd one model and super user

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+GENRE_CHOICES = (
+    ("sci-fi", "sci-fi"), 
+    ("adventure", "adventure"), 
+    ("comedy", "comedy"), 
+    ("romance", "romance"), 
+    ("drama", "drama"), 
+    ("documentary", "documentary")
+)
+
+class Movie(models.Model):
+    
+    img = models.CharField(max_length=250)
+    title = models.CharField(max_length=50)
+    genre = models.CharField(max_length=15, choices = GENRE_CHOICES)
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): 
+        return self.title
+    
+    class Meta: 
+        ordering = ['year']
+
