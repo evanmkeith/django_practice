f7e2b98895710ebb068ff68a50b1a7f3fe65baaf
ekeith <evanmkeith@gmail.com>
2022-04-09 13:40:52 -0700

    setup search option

---
+++ db/create_movies.sql
@@ -0,0 +1,8 @@
+INSERT INTO main_app_movie(img, title, genre, year, created_at)
+VALUES ('https://www.themoviedb.org/t/p/w300_and_h450_bestv2/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg', 'Free Guy', 'Comedy', 2021, '04-09-2022');
+INSERT INTO main_app_movie(img, title, genre, year, created_at)
+VALUES ('https://www.themoviedb.org/t/p/w300_and_h450_bestv2/d5NXSklXo0qyIYkgV94XAgMIckC.jpg', 'Dune', 'sci-fi', 2021, '04-09-2022');
+INSERT INTO main_app_movie(img, title, genre, year, created_at)
+VALUES ('https://www.themoviedb.org/t/p/w300_and_h450_bestv2/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg', 'Parasite', 'drama', 2019, '04-09-2022');
+INSERT INTO main_app_movie(img, title, genre, year, created_at)
+VALUES ('https://www.themoviedb.org/t/p/w300_and_h450_bestv2/fIE3lAGcZDV1G6XM5KmuWnNsPp1.jpg', 'Pulp Fiction', 'drama', 1994, '04-09-2022');
--- main_app/views.py
+++ main_app/views.py
@@ -4,21 +4,23 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Movie
 
 # Fake-a-base Movies
-class Movie: 
-    def __init__(self, title, genre, year): 
-        self.title = title
-        self.genre = genre
-        self.year = year
+# class Movie: 
+#     def __init__(self, title, genre, year): 
+#         self.title = title
+#         self.genre = genre
+#         self.year = year
 
-movies = [
-    Movie("The Abyss", "sci-fi", 1989), 
-    Movie("Indiana Jones: Raiders of the Lost Ark", "adventure", 1981), 
-    Movie("2001: A Space Odyssey ", "sci-fi", 1968), 
-    Movie("Interstellar", "sci-fi", 2014), 
-    Movie ("The Matrix", "sci-fi", 1999)
-]
+# movies = [
+#     Movie("The Abyss", "sci-fi", 1989), 
+#     Movie("Indiana Jones: Raiders of the Lost Ark", "adventure", 1981), 
+#     Movie("2001: A Space Odyssey ", "sci-fi", 1968), 
+#     Movie("Interstellar", "sci-fi", 2014), 
+#     Movie ("The Matrix", "sci-fi", 1999)
+# ]
+#End fake-a-base
 
 # Create your views here.
 class Index(TemplateView):
@@ -26,7 +28,7 @@ class Index(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["movies"] = movies
+        context["movies"] = Movie.objects.all()
         return context 
 
 class Home(TemplateView): 
